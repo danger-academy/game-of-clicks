d5b541efeeed9374202da79d26ba2ba81b2e1fbc
robert <robert.tarkenton@gmail.com>
2019-03-19 13:12:21 -0400

    Updates

---
+++ precache-manifest.97afc51d86729fec0eafe1835551e02a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dec3ed9fbaaa806c8186",
+    "url": "/game-of-clicks/static/js/runtime~main.ee7410c7.js"
+  },
+  {
+    "revision": "4664edc3f515490eb278",
+    "url": "/game-of-clicks/static/js/main.0e94c8a0.chunk.js"
+  },
+  {
+    "revision": "4c7ab43a199eaffcb672",
+    "url": "/game-of-clicks/static/js/2.da50c75f.chunk.js"
+  },
+  {
+    "revision": "4664edc3f515490eb278",
+    "url": "/game-of-clicks/static/css/main.ae17ee31.chunk.css"
+  },
+  {
+    "revision": "a7db0ab31ce6cb4de54b302310e2e97f",
+    "url": "/game-of-clicks/index.html"
+  }
+];
