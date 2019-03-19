3ff83365ff1337c0730e88bbf7adda1984abdfe1
robert <robert.tarkenton@gmail.com>
2019-03-19 12:46:18 -0400

    aesthetics and image links, game mechanics good

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Navbar = () => (
+    // console.log()
+    <div className="header">
+        <h1>Game of Clicks</h1>
+        <h2>Click an image to live, but do not click the same one twice!</h2>
+    </div>
+)
+
+export default Navbar;
